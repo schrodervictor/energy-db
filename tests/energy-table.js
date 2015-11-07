4fb76faa4dc26a826eefe8a8d3a567c6ad1d5703
Victor Schröder <schrodervictor@gmail.com>
2015-11-07 21:33:25 +0100

    Adds setKeySchema method to EnergyTable

--- tests/energy-table.js
+++ tests/energy-table.js
@@ -1,11 +1,26 @@
 var expect = require('chai').expect;
-
 var energyTable = require('../lib/energy-table');
 var EnergyTable = require('../lib/energy-table').EnergyTable;
 
 var dbMock = {};
 
+var tableInfo = {
+    Table: {
+        TableName: 'Sample-Table',
+        KeySchema: [
+            { AttributeName: 'some-hash-key', KeyType: 'HASH' },
+            { AttributeName: 'some-range-key', KeyType: 'RANGE' }
+        ],
+        AttributeDefinitions: [
+            { AttributeName: 'some-hash-key', AttributeType: 'S' },
+            { AttributeName: 'some-range-key', AttributeType: 'S' },
+            { AttributeName: 'some-other-attr', AttributeType: 'N' }
+        ]
+    }
+};
+
 describe('EnergyTable (module)', function() {
+
     describe('#getInstance(db, tableName, callback)', function() {
 
         it('should pass an EnergyTable instance to the callback', function(done) {
@@ -17,4 +32,22 @@ describe('EnergyTable (module)', function() {
         });
 
     });
+
+});
+
+describe('EnergyTable (class)', function() {
+
+    describe('#setKeySchema(tableInfo)', function() {
+
+        it('should set the table properties with keys information', function() {
+            var table = new EnergyTable(dbMock, 'Sample-Table');
+            table.setKeySchema(tableInfo);
+            expect(table.hashKey).to.equals('some-hash-key');
+            expect(table.hashKeyType).to.equals('S');
+            expect(table.rangeKey).to.equals('some-range-key');
+            expect(table.rangeKeyType).to.equals('S');
+        });
+
+    });
+
 });
