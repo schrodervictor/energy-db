2439f1ff01ffd1ebb62d58a8e6b69414249c803e
Victor Schröder <schrodervictor@gmail.com>
2015-11-07 20:11:18 +0100

    Adds describeTable method to EnergyDB and introduces sinon and sinon-chai as dependencies

--- tests/energy-db.js
+++ tests/energy-db.js
@@ -1,9 +1,29 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
 var energyDB = require('../lib/energy-db');
 var EnergyDB = energyDB.EnergyDB;
 var DynamoDB = require('aws-sdk').DynamoDB;
 
-var dynamoDBMock = {};
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+var dynamoDBMock = {
+    describeTable: sinon.spy(function(tableName, callback) {
+        var tableInfo = {
+            Table: {
+                TableName: tableName,
+                KeySchema: [
+                    { AttributeName: 'some-hash-key', KeyType: 'HASH' },
+                ],
+                AttributeDefinitions: [
+                    { AttributeName: 'some-hash-key', AttributeType: 'S' },
+                ]
+            }
+        };
+        return callback(null, tableInfo);
+    })
+};
 
 describe('energy-db', function() {
     describe('#connect(settings, callback)', function() {
@@ -34,6 +54,31 @@ describe('EnergyDB', function() {
             var db = new EnergyDB({}, dynamoDBMock);
             var connector = db.getConnector();
             expect(connector).to.equals(dynamoDBMock);
+        });
+    });
+
+    describe('#describeTable(tableName, callback)', function() {
+
+        afterEach(function() {
+            dynamoDBMock.describeTable.reset();
+        });
+
+        it('should forward the request to DynamoDB with the expected query format', function(done) {
+
+            var db = new EnergyDB({}, dynamoDBMock);
+            var tableName = 'Table-Name';
+            var expectedQuery = {TableName: tableName};
+
+            db.describeTable(tableName, function thisCallback(err, tableInfo) {
+                if (err) return done(err);
+                expect(dynamoDBMock.describeTable).to.have.been.calledOnce;
+                expect(dynamoDBMock.describeTable).to.have.been.calledWith(
+                    sinon.match(expectedQuery),
+                    thisCallback
+                );
+                done();
+            });
+
         });
 
     });
